9a6131b106f9e8e830fa2b7ce9f38a1e20b89bfd
George Pandelias <pandeliasg@gmail.com>
2025-01-20 21:09:00 +0200

    feat: Move questionnaire initialization to the PlayGameFragment, pass the username from gameIntro to PlayGameFragment

--- app/src/main/java/com/panco/multichoice/GameIntroFragment.kt
+++ app/src/main/java/com/panco/multichoice/GameIntroFragment.kt
@@ -1,7 +1,5 @@
 package com.panco.multichoice
 
-import DatabaseUtils
-import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,67 +8,33 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.widget.AppCompatEditText
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
 import androidx.navigation.findNavController
 import com.panco.multichoice.databinding.FragmentGameIntroBinding
-import com.panco.multichoice.models.Question
-import com.panco.multichoice.models.Questionnaire
-import com.panco.multichoice.repositories.QuestionRepository
-import com.panco.multichoice.viewModels.GameViewModel
 
 class GameIntroFragment : Fragment() {
     private var _binding: FragmentGameIntroBinding? = null
     private val binding get() = _binding!!
-    private lateinit var db: SQLiteDatabase
-    private val DB_NAME = "quiz-db"
-    private val QUESTIONS_SIZE: Int = 10
-    private val gameViewModel: GameViewModel by activityViewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
         _binding = FragmentGameIntroBinding.inflate(inflater, container, false)
         val view = binding.root
 
-        //Navigate to PlayGame fragment
-        val actionToPlayGame = GameIntroFragmentDirections.actionGameIntroFragmentToPlayGameFragment()
+        //Navigate to PlayGame fragment and passing the username IF GIVEN
         val btnStart = view.findViewById<Button>(R.id.btnStartGame)
         val etEnterUserName = view.findViewById<AppCompatEditText>(R.id.etUserName)
         btnStart.setOnClickListener {
-
-            if (etEnterUserName.text.toString().isEmpty()) {
+            val username = etEnterUserName.text.toString()
+            if (username.isEmpty()) {
                 Toast.makeText(view.context, "Παρακαλώ εισάγετε ένα όνομα χρήστη", Toast.LENGTH_SHORT).show()
             } else {
-                view.findNavController().navigate(actionToPlayGame)
+                val action = GameIntroFragmentDirections.actionGameIntroFragmentToPlayGameFragment(username)
+                view.findNavController().navigate(action)
             }
         }
-
-        //Copy database
-        DatabaseUtils.copyDatabase(requireContext(), DB_NAME)
-        //Open connection
-        db = SQLiteDatabase.openDatabase(
-            requireContext().getDatabasePath(DB_NAME).path,
-            null,
-            SQLiteDatabase.OPEN_READWRITE
-        )
-
-        val repo = QuestionRepository(db)
-        val questions: List<Question> = repo.getQuestionsByIds(repo.getRandomQuestionIds(QUESTIONS_SIZE))
-        println("\n\n\n\n\n---------------------------\n")
-        for (question in questions) {
-            println(question.id)
-            println(question.text)
-            question.answers.forEach {
-                println("${it.id} | ${it.text} | ${it.isCorrect} | ${it.questionId}")
-            }
-        }
-
-        val questionnaire = Questionnaire(questions)
-        gameViewModel.sharedData.value = questionnaire
-
         return view
     }
 
--- app/src/main/java/com/panco/multichoice/PlayGameFragment.kt
+++ app/src/main/java/com/panco/multichoice/PlayGameFragment.kt
@@ -1,5 +1,6 @@
 package com.panco.multichoice
 
+import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -9,33 +10,50 @@ import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import com.panco.multichoice.databinding.FragmentAboutBinding
 import com.panco.multichoice.databinding.FragmentPlayGameBinding
+import com.panco.multichoice.models.Question
 import com.panco.multichoice.models.Questionnaire
+import com.panco.multichoice.repositories.QuestionRepository
 import com.panco.multichoice.viewModels.GameViewModel
 
 
 class PlayGameFragment : Fragment() {
     private var _binding: FragmentPlayGameBinding? = null
     private val binding get() = _binding!!
-    private val gameViewModel: GameViewModel by activityViewModels()
-    private var receivedData: Questionnaire? = null
+    private lateinit var db: SQLiteDatabase
+    private val DB_NAME = "quiz-db"
+    private val QUESTIONS_SIZE: Int = 10
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
         _binding = FragmentPlayGameBinding.inflate(inflater, container, false)
         val view = binding.root
 
-        var questions: Questionnaire? = null
-        gameViewModel.sharedData.observe(viewLifecycleOwner){  data ->
-            receivedData = data
-           Log.d("target", "received data is: $data")
-            receivedData?.questions?.forEach { q ->
-                println(q.text)
+        val username = PlayGameFragmentArgs.fromBundle(requireArguments()).username //get the username
+
+        //Copy database
+        DatabaseUtils.copyDatabase(requireContext(), DB_NAME)
+        //Open connection
+        db = SQLiteDatabase.openDatabase(
+            requireContext().getDatabasePath(DB_NAME).path,
+            null,
+            SQLiteDatabase.OPEN_READWRITE
+        )
+
+        val repo = QuestionRepository(db)
+        val questions: List<Question> = repo.getQuestionsByIds(repo.getRandomQuestionIds(QUESTIONS_SIZE))
+        println("\n\n\n\n\n---------------------------\n") //TODO delete this
+        for (question in questions) {
+            println(question.id)
+            println(question.text)
+            question.answers.forEach {
+                println("${it.id} | ${it.text} | ${it.isCorrect} | ${it.questionId}")
             }
         }
+
+        val questionnaire = Questionnaire(questions)
 
 
         return view
