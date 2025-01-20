f9c7de9435be06573847bd80b4a5136a41e55425
George Pandelias <pandeliasg@gmail.com>
2025-01-20 23:44:45 +0200

    feat: Set the default and the selected styling for the buttons of the answers

--- app/src/main/java/com/panco/multichoice/PlayGameFragment.kt
+++ app/src/main/java/com/panco/multichoice/PlayGameFragment.kt
@@ -1,11 +1,15 @@
 package com.panco.multichoice
 
 import android.database.sqlite.SQLiteDatabase
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import com.panco.multichoice.databinding.FragmentPlayGameBinding
 import com.panco.multichoice.models.Question
 import com.panco.multichoice.models.Questionnaire
@@ -55,13 +59,51 @@ class PlayGameFragment : Fragment() {
         val questionnaire = Questionnaire(questions)
         val currentPosition: Int= 0
         val q: Question  = questionnaire.questions[currentPosition]
+        resetOptionsStyling()//todo it is not running
         binding.tvQuestion.text = q.text
         binding.tvOptionOne.text = q.answers[0].text
         binding.tvOptionTwo.text = q.answers[1].text
         binding.tvOptionThree.text = q.answers[2].text
         binding.tvOptionFour.text = q.answers[3].text
 
+        binding.tvOptionOne.setOnClickListener {
+            selectOptionView(binding.tvOptionOne, 1)
+        }
+        binding.tvOptionTwo.setOnClickListener {
+            selectOptionView(binding.tvOptionTwo, 2)
+        }
+        binding.tvOptionThree.setOnClickListener {
+            selectOptionView(binding.tvOptionThree, 3)
+        }
+        binding.tvOptionFour.setOnClickListener {
+            selectOptionView(binding.tvOptionFour, 4)
+        }
         return view
+    }
+
+
+    /** Resets the options back to the default style
+     */
+    private fun resetOptionsStyling() {
+        val options = ArrayList<TextView>()
+        options.add(0, binding.tvOptionOne)
+        options.add(1, binding.tvOptionTwo)
+        options.add(2, binding.tvOptionThree)
+        options.add(3, binding.tvOptionFour)
+
+        options.forEach{ option ->
+            println("Option $option")
+            option.setTextColor(Color.parseColor("#7A8089"))
+            option.typeface = Typeface.DEFAULT
+            option.background = view?.let { ContextCompat.getDrawable(it.context, R.drawable.option_border_bg_default) }
+        }
+    }
+
+    private fun selectOptionView(tv: TextView, selectedOption: Int) {
+        resetOptionsStyling()
+        tv.setTextColor(Color.parseColor("#363A43"))
+        tv.setTypeface(tv.typeface, Typeface.BOLD)
+        tv.background = view?.let { ContextCompat.getDrawable(it.context, R.drawable.option_border_bg_selected) }
     }
 
 }
