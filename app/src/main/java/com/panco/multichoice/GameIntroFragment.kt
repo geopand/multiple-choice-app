bfa15246eb77ac9aadfbf21eee740388390fd045
George Pandelias <pandeliasg@gmail.com>
2025-01-20 20:33:20 +0200

    feat: Guard the start of the game by checking if the user entered a username

--- app/src/main/java/com/panco/multichoice/GameIntroFragment.kt
+++ app/src/main/java/com/panco/multichoice/GameIntroFragment.kt
@@ -1,15 +1,17 @@
 package com.panco.multichoice
 
+import DatabaseUtils
 import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.findNavController
-import com.panco.multichoice.databinding.FragmentAboutBinding
 import com.panco.multichoice.databinding.FragmentGameIntroBinding
 import com.panco.multichoice.models.Question
 import com.panco.multichoice.models.Questionnaire
@@ -36,8 +38,14 @@ class GameIntroFragment : Fragment() {
         //Navigate to PlayGame fragment
         val actionToPlayGame = GameIntroFragmentDirections.actionGameIntroFragmentToPlayGameFragment()
         val btnStart = view.findViewById<Button>(R.id.btnStartGame)
+        val etEnterUserName = view.findViewById<AppCompatEditText>(R.id.etUserName)
         btnStart.setOnClickListener {
-            view.findNavController().navigate(actionToPlayGame)
+
+            if (etEnterUserName.text.toString().isEmpty()) {
+                Toast.makeText(view.context, "Παρακαλώ εισάγετε ένα όνομα χρήστη", Toast.LENGTH_SHORT).show()
+            } else {
+                view.findNavController().navigate(actionToPlayGame)
+            }
         }
 
         //Copy database
